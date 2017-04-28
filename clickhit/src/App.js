eb69ecda3589c2423d4f3e44dbeff4378d36a9e1
Sneh <sneh@SnowbookPro.local>
2017-04-28 14:26:19 +0200

    first code

--- clickhit/src/App.js
+++ clickhit/src/App.js
@@ -7,10 +7,17 @@ class App extends Component {
   render() {
     return (
             <div className="App">
-            <Grid>
+            <Grid className="headBar" fluid="true">
                 <Row>
-                <Col md={4}>Čtyřka</Col>
-                <Col md={8}>Osmička</Col>
+                <Col md={4}>levy roh</Col>
+                <Col md={4}><div className="logo">ClickHit!</div></Col>
+                <Col md={4}>
+                    <Row>
+                        <Col md={6}>9/15</Col>
+                        <Col md={6} className="score">5344</Col>
+                    </Row>
+                </Col>
+                
                 </Row>
                
                 
